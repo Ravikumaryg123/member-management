8802066f30bbc46e4fb2842923ed982efc895d63
Ravikumar Y G <ravikumar.gangaboregowda@tietoevry.com>
2025-11-25 07:07:34 +0530

    Added validation

--- src/main/java/com/surest/member/dto/LoginDto.java
+++ src/main/java/com/surest/member/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package com.surest.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class LoginDto {
 
+    @NotBlank(message = "User name must not be blank")
     private String usernameOrEmail;
+
+    @NotBlank(message = "Password must not be blank")
     private String password;
 }
